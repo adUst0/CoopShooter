233b0b7ea6896cdbba5bc4eb701096ef1b4d9eff
Boris Ivanov <boris.iv.iv@gmail.com>
2021-09-05 21:58:46 +0300

    Implemented SProjectileWeapon and Granade launcher

---
+++ Source/CoopShooter/Private/SProjectileWeapon.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SProjectileWeapon.h"
+
+void ASProjectileWeapon::Fire()
+{
+    AActor* MyOwner = GetOwner();
+    if (!MyOwner || !ProjectileClass) return;
+
+    FVector EyeLocation;
+    FRotator EyeRotation;
+    MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+    const FVector MuzzleLocation = MeshComponent->GetSocketLocation(MuzzleSocketName);
+
+    FActorSpawnParameters SpawnParameters;
+    SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+    GetWorld()->SpawnActor<AActor>(ProjectileClass, MuzzleLocation, EyeRotation, SpawnParameters);
+}
